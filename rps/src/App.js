c9ada0b91dc9e30f793588ebc99840e169a8dd85
randleem <erandle2211@gmail.com>
2021-01-28 14:51:41 +0000

    test

---
+++ rps/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+const [playersChoice, setPlayersChoice] = useState(null);
+const [computersChoice, setComputersChoice] = useState(null);
+const rps = [rock, paper, scissors];
+
+
+  return (
+    
+  )
+}
+
+export default App;
