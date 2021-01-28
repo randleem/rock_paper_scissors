b1eb0ba9523d45858f96719d77a280fc3a5a9541
= <=>
2021-01-28 14:55:01 +0000

    test 3

--- rps/src/index.js
+++ rps/src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-//testy
+//testy2
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
