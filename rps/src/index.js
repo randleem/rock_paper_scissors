4aa2d7cccc324b719488e38f552fc9273b1aa620
randleem <erandle2211@gmail.com>
2021-01-28 15:35:36 +0000

    branch name change?

--- rps/src/index.js
+++ rps/src/index.js
@@ -16,3 +16,41 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// git filter-branch --env-filter '
+// OLD_EMAIL="="
+// CORRECT_NAME="randleem"
+// CORRECT_EMAIL="erandle2211@gmail.com"
+// if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+// then
+//     export GIT_COMMITTER_NAME="$CORRECT_NAME"
+//     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+// fi
+// if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+// then
+//     export GIT_AUTHOR_NAME="$CORRECT_NAME"
+//     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+// fi
+// ' --tag-name-filter cat -- --branches --tags
+
+// git filter-branch --env-filter 'if [ "$GIT_AUTHOR_EMAIL" = "=" ]; then
+//      GIT_AUTHOR_EMAIL="erandle2211@gmail.com";
+//      GIT_AUTHOR_NAME="randleem";
+//      GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL;
+//      GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all
+
+
+
+// git filter-branch --commit-filter '
+//         if [ "$GIT_COMMITTER_NAME" = "<Old Name>" ];
+//         then
+//                 GIT_COMMITTER_NAME="<New Name>";
+//                 GIT_AUTHOR_NAME="<New Name>";
+//                 GIT_COMMITTER_EMAIL="<New Email>";
+//                 GIT_AUTHOR_EMAIL="<New Email>";
+//                 git commit-tree "$@";
+//         else
+//                 git commit-tree "$@";
+//         fi' HEAD
+
+
